0fd3f1586d370de6b34ef95117325f87d6f5ed95
Emily Briaguez-Agurto <emilyb.agurto@gmail.com>
2022-08-31 21:34:16 -0400

    Homework complete

---
+++ src/main/java/Notes.java
@@ -0,0 +1,55 @@
+// Class name MUST match file name and
+// MUST contain a main () method
+public class Notes {
+    public static void main(String[] args) {
+       // Java output
+       // System.out.println();
+
+       // Types of variables
+       // String
+        System.out.println("This is a string.");
+
+        // Integer
+        System.out.println(1);
+
+        // Float
+        System.out.println(2.00f);
+
+        // Char
+        System.out.println('E');
+
+        // Boolean
+        System.out.println(true);
+
+        // Combine both text and variable
+        String name = "Emily";
+        System.out.println("Hi my name is " + name);
+
+        // Combine variable and variable
+        int x = 5;
+        int y = 5;
+        System.out.println(x + y);
+
+        // Declare multiple variables in one line
+        String firstName = "Emily", middleName = "lizett", lastName = "Briaguez-Agurto";
+        System.out.println(firstName + middleName + lastName);
+
+        // More Data Types
+        // Byte
+        System.out.println(20);
+
+        // Short
+        System.out.println(1000);
+
+        // Long
+        System.out.println(100000L);
+
+        // Double
+        System.out.println(5.020d);
+
+
+
+
+
+    }
+}
